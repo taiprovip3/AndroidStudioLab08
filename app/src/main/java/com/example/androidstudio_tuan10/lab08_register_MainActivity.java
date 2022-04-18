5c6d5602ae988ee0b46fd546b47066fa7b874510
taiproduaxe <taito1doraemon@gmail.com>
2022-04-18 14:23:00 +0700

    done

---
+++ app/src/main/java/com/example/androidstudio_tuan10/lab08_register_MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidstudio_tuan10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lab08_register_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab08_register__main);
+    }
+}
---
+++ app/src/main/java/com/example/androidstudio_tuan10/lab08_screen1_MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.androidstudio_tuan10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class lab08_screen1_MainActivity extends AppCompatActivity {
+    Button btnSignin;
+    Button btnRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab08_screen1__main);
+
+        btnSignin = (Button) findViewById(R.id.screen1_btnSignin);
+        btnRegister = (Button) findViewById(R.id.screen1_btnRegister);
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(context, MainActivity.class);
+                Intent intent = new Intent(lab08_screen1_MainActivity.this, lab08_signin_MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(lab08_screen1_MainActivity.this, lab08_register_MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
